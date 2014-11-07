235a6fbddaf122c67b927fe932cbe95c906984ad
16chuang <16chuang@castilleja.org>
2014-11-07 09:57:12 -0800

    Assign 6 - encoding (step 4) complete.

--- assign6/Huffman/src/encoding.cpp
+++ assign6/Huffman/src/encoding.cpp
@@ -11,6 +11,7 @@
 void makePriorityQueue(PriorityQueue<HuffmanNode*>& pqueue, const Map<int, int>& freqTable); 
 void makeTree(PriorityQueue<HuffmanNode*>& pqueue);
 void traverseTree(HuffmanNode* currNode, Map<int, string>& encodingMap, string currCode);
+void writeBit(obitstream& output, string value);
 
 /**
  * @brief buildFrequencyTable
@@ -100,11 +101,11 @@ void makePriorityQueue(PriorityQueue<HuffmanNode*>& pqueue, const Map<int, int>&
  * Builds encoding map of characters and their codes by calling recursive 
  * traverseTree function. 
  * @param encodingTree - pointer to first node in encoding tree from previous step
- * @return 
+ * @return - encoding map. Empty map if pointer to first node in tree is null.
  */
 Map<int, string> buildEncodingMap(HuffmanNode* encodingTree) {
     Map<int, string> encodingMap;
-    traverseTree(encodingTree, encodingMap, "");    
+    if(encodingTree != NULL) traverseTree(encodingTree, encodingMap, "");    
     return encodingMap;
 }
 
@@ -126,8 +127,44 @@ void traverseTree(HuffmanNode* currNode, Map<int, string>& encodingMap, string c
     traverseTree(currNode->one, encodingMap, currCode + "1"); // right
 }
 
+/**
+ * @brief encodeData
+ * Goes through input file and creates Huffman encoding for file contents based
+ * on previously made encoding map for each character. Writes Huffman encoding to 
+ * output. 
+ * @param input - input file or string
+ * @param encodingMap - map of characters and their codes; previously created.
+ * @param output - output stream where Huffman encoding is written
+ */
 void encodeData(istream& input, const Map<int, string>& encodingMap, obitstream& output) {
-    // TODO: implement this function
+    char currCharacter;
+    bool endOfFile = false;
+    
+    while (!endOfFile) {
+        currCharacter = input.get();
+        if (currCharacter == -1) { // end of file
+            writeBit(output, encodingMap.get(PSEUDO_EOF));
+            endOfFile = true;
+        } else {
+            writeBit(output, encodingMap.get(currCharacter));
+        }
+    }
+}
+
+/**
+ * @brief writeBit
+ * Given a string code of 0's and 1's, writes the appropriate bits to output stream.
+ * @param output - output stream to write bits
+ * @param value - string code to convert into bits
+ */
+void writeBit(obitstream& output, string value) {
+    for (int i = 0; i < value.length(); i++) {
+        if (value[i] == '0') {
+            output.writeBit(0);
+        } else if (value[i] == '1') {
+            output.writeBit(1);
+        }
+    }
 }
 
 void decodeData(ibitstream& input, HuffmanNode* encodingTree, ostream& output) {
