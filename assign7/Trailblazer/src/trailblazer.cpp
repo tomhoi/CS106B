e36e71fb061ec4827432bcb74b40de89f8c4a0a5
16chuang <16chuang@castilleja.org>
2014-11-27 22:42:28 -0800

    Assign 7 - DFS complete.

--- assign7/Trailblazer/src/trailblazer.cpp
+++ assign7/Trailblazer/src/trailblazer.cpp
@@ -5,16 +5,58 @@
 
 #include "costs.h"
 #include "trailblazer.h"
-// TODO: include any other headers you need; remove this comment
 using namespace std;
 
+bool DFS(BasicGraph& graph, Vertex* curr, Vertex* end, Vector<Vertex*>& path);
+
+/**
+ * @brief depthFirstSearch
+ * Wrapper function for recursive DFS on given graph. Returns the found (or not) path
+ * as a Vector of vertex pointers.
+ * @param graph - Provided graph on which to search
+ * @param start - Start vertex of path
+ * @param end - End vertex of path
+ * @return - The found path as a Vector of vertex pointers
+ */
 Vector<Vertex*> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+    graph.resetData(); // reset before each search
     Vector<Vertex*> path;
+    DFS(graph, start, end, path); // search
     return path;
+}
+
+/**
+ * @brief DFS
+ * Recursive depth first search on given graph. For each vertex it visits, DFS marks 
+ * it, searches its neighbors, and removes marking if vertex proved unfruitful. DFS
+ * goes deep; it goes down one particular path as far as possible before backtracking.
+ * @param graph - Provided graph on which to search
+ * @param curr - Current vertex
+ * @param end - Desired end vertex of the path
+ * @param path - The found path as a Vector of vertex pointers
+ * @return 
+ */
+bool DFS(BasicGraph& graph, Vertex* curr, Vertex* end, Vector<Vertex*>& path) {    
+    // add curr vertex
+    curr->visited = true;
+    curr->setColor(GREEN);
+    path.add(curr);
+    
+    // base case
+    if (curr == end) return true;
+    
+    // for each univisited neighbor
+    for (Edge* edge : curr->edges) {
+        Vertex* neighbor = edge->finish; // extract neighbor
+        if (neighbor->visited) continue; // skip if already visited
+        if (DFS(graph, neighbor, end, path)) return true; // make choice
+        // otherwise unmake choice (nothing to do)
+    }
+    
+    // (partially) undo curr vertex choice
+    curr->setColor(GRAY);
+    path.remove(path.size() - 1);
+    return false;
 }
 
 Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
