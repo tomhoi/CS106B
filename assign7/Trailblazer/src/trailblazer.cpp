55027f9223d27b1542058e8b6d3f73e27a57bfb4
16chuang <16chuang@castilleja.org>
2014-11-28 09:35:22 -0800

    Assign 7 - BFS complete.

--- assign7/Trailblazer/src/trailblazer.cpp
+++ assign7/Trailblazer/src/trailblazer.cpp
@@ -5,9 +5,11 @@
 
 #include "costs.h"
 #include "trailblazer.h"
+#include "queue.h"
 using namespace std;
 
 bool DFS(BasicGraph& graph, Vertex* curr, Vertex* end, Vector<Vertex*>& path);
+void retracePath(Vertex* curr, Vector<Vertex*>& path);
 
 /**
  * @brief depthFirstSearch
@@ -59,13 +61,66 @@ bool DFS(BasicGraph& graph, Vertex* curr, Vertex* end, Vector<Vertex*>& path) {
     return false;
 }
 
+/**
+ * @brief breadthFirstSearch
+ * Breadth first search for a given graph. Goes through graph's current generation
+ * of neighbors (all the same distance away) and enqueues the next generation for
+ * future processing. Guaranteed to find the shortest path (assuming all paths are equal
+ * in cost). Goes wide across all possible neighbors.
+ * @param graph - Provided graph on which to search
+ * @param start - Start vertex of path
+ * @param end - Desired end vertex of path
+ * @return - The found path as a Vector of vertex pointers
+ */
 Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+    graph.resetData(); // reset before each search
     Vector<Vertex*> path;
+    Queue<Vertex*> frontier;
+    
+    // first vertex
+    start->visited = true;
+    start->setColor(YELLOW);
+    frontier.enqueue(start);
+    
+    // search
+    while(!frontier.isEmpty()) {
+        Vertex* curr = frontier.dequeue(); // dequeue current generation
+        curr->setColor(GREEN);
+        
+        for (Edge* edge : curr->edges) { // next generation neighbors
+            Vertex* neighbor = edge->finish; // extract neighbor
+            if (neighbor->visited) continue; // skip if already visited
+            
+            frontier.enqueue(neighbor); // enqueue
+            neighbor->previous = curr;
+            neighbor->visited = true;
+            neighbor->setColor(YELLOW);
+            
+            if (neighbor == end) { // path found
+                neighbor->setColor(GREEN);
+                retracePath(neighbor, path);
+                return path;
+            }
+        }
+    }
+    
     return path;
+}
+
+/**
+ * @brief retracePath
+ * Retraces found path given the end index of the path.
+ * @param curr - End index of the found path
+ * @param path - Vector to fill with vertexes on found path 
+ */
+void retracePath(Vertex* curr, Vector<Vertex*>& path) {
+    Vector<Vertex*> temp;
+    for (curr; curr != NULL; curr = curr->previous) { // retrace
+        temp.add(curr);
+    }   
+    for (int i = temp.size() - 1; i >= 0; i--) { // flip the vector so start index is at 0
+        path.add(temp[i]);
+    }
 }
 
 Vector<Vertex*> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end) {
