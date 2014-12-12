805957b585522590c7f10f45875ceee90b02d2f9
16chuang <16chuang@castilleja.org>
2014-12-11 23:15:05 -0800

    Assign 7 - Kruskal's algorithm complete, assignment complete.

--- assign7/Trailblazer/src/trailblazer.cpp
+++ assign7/Trailblazer/src/trailblazer.cpp
@@ -7,6 +7,8 @@
 #include "trailblazer.h"
 #include "queue.h"
 #include "pqueue.h"
+#include "set.h"
+#include "map.h"
 using namespace std;
 
 bool DFS(BasicGraph& graph, Vertex* curr, Vertex* end, Vector<Vertex*>& path);
@@ -14,6 +16,9 @@ void retracePath(Vertex* curr, Vector<Vertex*>& path);
 Vector<Vertex*> genericDijkstra(BasicGraph& graph, Vertex* start, Vertex* end, double (newPriority)(Vertex*, Vertex*, double));
 double newDijkstraPriority(Vertex* start, Vertex* end, double cost);
 double newAStarPriority(Vertex* start, Vertex* end, double cost);
+bool inSameSet(Vertex* start, Vertex* end, Vector<Set<Vertex*> >& vertexSets);
+void mergeSets(Vertex* start, Vertex* end, Vector<Set<Vertex*> >& vertexSets);
+Vertex* parent(Vertex* curr);
 
 /**
  * @brief depthFirstSearch
@@ -113,7 +118,7 @@ Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end
 
 /**
  * @brief retracePath
- * Retraces found path given the end index of the path.
+ * Recursively retraces found path given the end index of the path.
  * @param curr - End index of the found path
  * @param path - Vector to fill with vertexes on found path 
  */
@@ -124,6 +129,7 @@ void retracePath(Vertex* curr, Vector<Vertex*>& path) {
 }
 
 Vector<Vertex*> genericDijkstra(BasicGraph& graph, Vertex* start, Vertex* end, double (newPriority)(Vertex*, Vertex*, double)) {
+    // ================== SETUP ===================
     Vector<Vertex*> path;
     PriorityQueue<Vertex*> pqueue;
     
@@ -139,7 +145,7 @@ Vector<Vertex*> genericDijkstra(BasicGraph& graph, Vertex* start, Vertex* end, d
     start->cost = 0;
     pqueue.enqueue(start, newPriority(start, end, 0));
     
-    // search
+    // ================== SEARCH ==================
     while (!pqueue.isEmpty()) {
         Vertex* curr = pqueue.dequeue(); // dequeue highest priority node
         curr->visited = true;
@@ -150,6 +156,7 @@ Vector<Vertex*> genericDijkstra(BasicGraph& graph, Vertex* start, Vertex* end, d
             return path;
         }
         
+    // ================== RELAX ===================
         for (Edge* edge : curr->edges) { // neighbors
             Vertex* neighbor = edge->finish; // extract neighbor
             if (neighbor->visited) continue;
@@ -170,6 +177,14 @@ Vector<Vertex*> genericDijkstra(BasicGraph& graph, Vertex* start, Vertex* end, d
     return path;
 }
 
+/**
+ * @brief dijkstrasAlgorithm
+ * Uses Dijkstra's algorithm to search a given graph. [FINISH MEEEEEEEEEEEEEE---------]
+ * @param graph - Provided graph on which to search
+ * @param start - Start vertex of path
+ * @param end - Desired end vertex of path
+ * @return - The found path as a Vector of vertex pointers
+ */
 Vector<Vertex*> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end) {
     return genericDijkstra(graph, start, end, newDijkstraPriority);
 }
@@ -178,19 +193,168 @@ double newDijkstraPriority(Vertex* start, Vertex* end, double cost) {
     return cost;
 }
 
-double newAStarPriority(Vertex* start, Vertex* end, double cost) {
-    return cost + heuristicFunction(start, end);
-}
-
+/**
+ * @brief aStar
+ * Uses A* algorithm to search a given graph. Similar to Dijkstra's, but uses a heuristic
+ * function when calculating the new priority of a node so that it can direct its search
+ * more towards the end (vs. searching in all directions in concentric-ish circle as with
+ * Dijkstra's). Calls generic Dijkstra function with different priority calculating callback
+ * function.
+ * @param graph - Provided graph on which to search
+ * @param start - Start vertex of path
+ * @param end - Desired end vertex of path
+ * @return - The found path as a Vector of vertex pointers
+ */
 Vector<Vertex*> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
     return genericDijkstra(graph, start, end, newAStarPriority);
 }
 
+/**
+ * @brief newAStarPriority
+ * Callback function for calculating new node priorities during A* search. Combines actual cost
+ * (of previous node's cost + cost of new edge) and a heuristic function that calculates the 
+ * straight distance from the current (start) node to the end of the desired path. Added heuristic
+ * makes sure that the algorithm fist explores paths closer towards the end point.
+ * @param start - Current vertex being assigned a priority
+ * @param end - Desired end vertex of path
+ * @param cost - Previous vertex's cost + cost of new edge to current vertex
+ * @return 
+ */
+double newAStarPriority(Vertex* start, Vertex* end, double cost) {
+    return cost + heuristicFunction(start, end);
+}
+
+/**
+ * @brief kruskal
+ * Uses Kruskal's algorithm to generate minimum spanning tree for a given weighted graph.
+ * Examines each edge starting from the least weighted one. If the edge joins vertices that
+ * haven't already been connected by the MST, then adds edge to the MST and joins the vertices'
+ * clusters (other vertices it is connected to by the existing MST). If the edge joins vertices
+ * that are already connected, the algorithm ignores those edges. Continues until no more edges
+ * left to examine.
+ * @param graph - Provided graph from which to create a MST
+ * @return - Set of edges to be included in the MST
+ */
 Set<Edge*> kruskal(BasicGraph& graph) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty set so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+    // ================== SETUP ===================
     Set<Edge*> mst;
+    Set<Edge*> allEdges = graph.getEdgeSet();
+    Set<Vertex*> allVertices = graph.getVertexSet();
+    
+    PriorityQueue<Edge*> pq; // put edges and weights in a priority queue
+    for (Edge* edge : allEdges) { 
+        pq.enqueue(edge, edge->cost);
+    }
+    
+   Vector<Set<Vertex*> > vertexSets; // put all vertices into their own cluster
+    for (Vertex* vertex : allVertices) {
+        Set<Vertex*> newEmptySet;
+        newEmptySet.add(vertex);
+        vertexSets.add(newEmptySet);
+    }
+    
+    // ================ MAKE TREE =================
+    while (!pq.isEmpty()) {
+        Edge* edge = pq.dequeue();
+        // if vertices aren't in the same cluster, add to MST and merge their clusters
+        if (!inSameSet(edge->start, edge->finish, vertexSets)) {
+            mst.add(edge);
+            mergeSets(edge->start, edge->finish, vertexSets);
+        }
+    }
+    
     return mst;
 }
+
+/**
+ * @brief inSameSet
+ * Checks if start and end vertices of an edge are the same cluster of connected
+ * vertices.
+ * @param start - Start vertex of edge being examined
+ * @param end - End vertex of edge being examined
+ * @param vertexSets - Vector of vertex clusters
+ * @return - Returns true if vertices are in the same cluster
+ */
+bool inSameSet(Vertex* start, Vertex* end, Vector<Set<Vertex*> >& vertexSets) {
+    for (Set<Vertex*> set : vertexSets) {
+        if (set.contains(start) && set.contains(end)) return true;
+    }
+    return false;
+}
+
+/**
+ * @brief mergeSets
+ * Merges the clusters of two vertices (that aren't already in the same set).
+ * @param start - Vertex in one cluster
+ * @param end - Vertex in another cluster
+ * @param vertexSets - Vector of vertex clusters
+ */
+void mergeSets(Vertex* start, Vertex* end, Vector<Set<Vertex*> >& vertexSets) {
+    int startSet, endSet;
+    
+    // find sets
+    Set<Vertex*> curr;
+    for (int i = 0; i < vertexSets.size(); i++) {
+        curr = vertexSets[i];
+        if (curr.contains(start)) {
+            startSet = i;
+        } else if (curr.contains(end)) {
+            endSet = i;
+        }
+    }
+   
+    // merge
+    vertexSets[startSet] += vertexSets[endSet];
+    vertexSets.remove(endSet);
+}
+
+/*
+ * 
+// * Same basic idea as Kruskal's algorithm implementation above, except uses
+// * a disjoint set forest instead of a Vector of vertex sets. Clusters are trees
+// * with the previous pointers of each vertex pointing either to itself (if it is the
+// * head of the tree) or the parent node on the tree. Merging trees requires rewiring
+// * of one tree's head to point to the other tree's head. Checking if vertices are in
+// * the same tree requires comparing the vertices' heads and seeing if they are the same.
+Set<Edge*> kruskal(BasicGraph& graph) {
+    // ================== SETUP ===================
+    Set<Edge*> mst;
+    Set<Edge*> allEdges = graph.getEdgeSet();
+    Set<Vertex*> allVertices = graph.getVertexSet();
+    
+    // put edges and weights in a priority queue
+    PriorityQueue<Edge*> pq;
+    for (Edge* edge : allEdges) { 
+        pq.enqueue(edge, edge->cost);
+    }
+    
+    // each vertex is its own disjoint set; its parent is itself
+    for (Vertex* vertex : allVertices) vertex->previous = vertex;
+    
+    // ================ MAKE TREE =================
+    while (!pq.isEmpty()) {
+        Edge* edge = pq.dequeue();
+        
+        // if vertices aren't in the same disjoint set, add the edge to the MST and merge the sets
+        Vertex* startParent = parent(edge->start);
+        Vertex* finishParent = parent(edge->finish);
+        if (startParent != finishParent) { 
+            mst.add(edge);
+            startParent->previous = finishParent; // merge disjoint sets
+        }
+    }
+    
+    return mst;
+}
+
+// * Recursively finds the head vertex of the tree of a given vertex.
+// * @param curr - Given vertex that wants to find the head of its tree
+// * @return - Head vertex
+Vertex* parent(Vertex* curr) {
+    if (curr->previous == curr) { // at the head of the disjoint set
+        return curr;
+    } else {
+        return parent(curr->previous); // keep going up the set
+    }
+}
+*/
